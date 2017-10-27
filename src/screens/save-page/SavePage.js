025b29a0025b78c61da473455486764c3495fc68
farid tanwier <varid68@gmail.com>
2017-10-28 03:41:11 +0700

    Update SavePage.js
    fixing delete all saved time

--- src/screens/save-page/SavePage.js
+++ src/screens/save-page/SavePage.js
@@ -48,6 +48,7 @@ export default class SavePage extends React.Component {
     return (
       <Container>
         <Header
+          get={this.getStorage.bind(this)}
           pop={this.pop.bind(this)} />
 
         {saved != null
