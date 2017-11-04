083bdd8ee3726ac31483407ad1d2e64e85c2e986
farid tanwier <varid68@gmail.com>
2017-11-05 02:27:56 +0700

    Fixing: Can't scroll in ListView saved time

--- src/screens/save-page/components/ModalContent.js
+++ src/screens/save-page/components/ModalContent.js
@@ -9,6 +9,7 @@ export default class ModalContent extends React.Component {
 
     this.state = {
       pan: new Animated.ValueXY(),
+      datas: []
     };
   }
 
@@ -16,6 +17,12 @@ export default class ModalContent extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.modalVisible != this.props.modalVisible) {
       this.state.pan.setOffset({ y: 270 });
+    }
+
+    if (nextProps.modalVisible) {
+      this.setState({
+        datas: this.props.datas[nextProps.indexList]
+      });
     }
   }
 
@@ -57,6 +64,7 @@ export default class ModalContent extends React.Component {
     }];
   }
 
+
   async deleteItem(date) {
     let saved = await AsyncStorage.getItem('saved');
     let savedAsJson = JSON.parse(saved);
@@ -69,26 +77,31 @@ export default class ModalContent extends React.Component {
         }
       });
     }
-    this.props.closeModal(false);
+    this.props.closeModal(false, 0);
     this.props.getStorage();
   }
 
 
+  displayTime(filterKey) {
+    const result = Object.keys(this.state.datas).filter((key) => {
+      return (key === 'time');
+    }).map(() => {
+      return this.state.datas.time[filterKey];
+    });
+
+    return result;
+  }
+
+
   render() {
-    let { sunrise, sunset, solar_noon, day_length, civil_twilight_begin, civil_twilight_end,
-      nautical_twilight_begin, nautical_twilight_end, astronomical_twilight_begin, astronomical_twilight_end } = this.props.terbit.time;
-
-    let { date } = this.props.terbit;
-
     return (
       <Modal
         animationType='slide'
         transparent={true}
         visible={this.props.modalVisible}
-        onRequestClose={() => this.props.closeModal(false)}>
+        onRequestClose={() => this.props.closeModal(false, 0)}>
 
         <Animated.View style={this.getStyle()} {...this.panResponder.panHandlers}>
-          {/* <View style={{ backgroundColor: '#fff', flex: 1 }}> */}
           <Header
             style={{ backgroundColor: '#009688' }}
             androidStatusBarColor='#00897B'>
@@ -98,62 +111,55 @@ export default class ModalContent extends React.Component {
             <Right>
               <Button
                 transparent
-                onPress={() => this.deleteItem(date)}
+                onPress={() => this.deleteItem(this.state.datas.date)}
                 style={{ width: 60, }}>
                 <Icon name='trash' />
               </Button>
               <Button
                 transparent
                 style={{ width: 60 }}
-                onPress={() => this.props.closeModal(false)}>
+                onPress={() => this.props.closeModal(false, 0)}>
                 <Icon name='close' />
               </Button>
             </Right>
           </Header>
 
-          <View style={{ padding: 10, backgroundColor: '#fff' }}>
-            <Text style={{ color: '#009688', fontSize: 16 }}>Tanggal Waktu Terbit Terbenam</Text>
-            <Text style={{ marginBottom: 10 }}>{date}</Text>
-
-            <Text style={{ color: '#009688', fontSize: 16 }}>Lokasi Pengecekan Waktu</Text>
-            <Text style={{ marginBottom: 10 }}>{this.props.terbit.address}</Text>
-
+          <View style={{ backgroundColor: '#fff', padding: 10 }}>
+            <Text style={{ color: '#009688' }}>Waktu Terbit Terbenam</Text>
+            <Text style={{ fontSize: 16 }}>{this.state.datas.date}</Text>
             <View style={styles.lineView}>
               <Text style={styles.line}></Text>
             </View>
+            <Text style={{ marginTop: 20 }}>Waktu Matahari Terbit</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('sunrise')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Matahari Terbit</Text>
-            <Text style={{ color: '#000' }}>{sunrise}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Matahari Terbenam</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('sunset')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Matahari Terbenam</Text>
-            <Text style={{ color: '#000' }}>{sunset}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Matahari Tepat di atas (Tengah Hari)</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('solar_noon')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Matahari Tepat di atas (Tengah Hari)</Text>
-            <Text style={{ color: '#000' }}>{solar_noon}</Text>
+            <Text style={{ marginTop: 15 }}>Panjang Waktu Matahari Bersinar</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('day_length')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Panjang Waktu Matahari Bersinar</Text>
-            <Text style={{ color: '#000' }}>{day_length}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Civil Twilight Dimulai</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('civil_twilight_begin')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Civil Twilight Dimulai</Text>
-            <Text style={{ color: '#000' }}>{civil_twilight_begin}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Civil Twilight Berakhir</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('civil_twilight_end')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Civil Twilight Berakhir</Text>
-            <Text style={{ color: '#000' }}>{civil_twilight_end}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Nautical Twilight Dimulai</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('nautical_twilight_begin')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Nautical Twilight Dimulai</Text>
-            <Text style={{ color: '#000' }}>{nautical_twilight_begin}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Nautical Twilight Berakhir</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('nautical_twilight_end')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Nautical Twilight Berakhir</Text>
-            <Text style={{ color: '#000' }}>{nautical_twilight_end}</Text>
+            <Text style={{ marginTop: 15 }}>Waktu Astronomical Twilight Dimulai</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('astronomical_twilight_begin')}</Text>
 
-            <Text style={{ marginTop: 10 }}>Waktu Astronomical Twilight Dimulai</Text>
-            <Text style={{ color: '#000' }}>{astronomical_twilight_begin}</Text>
-
-            <Text style={{ marginTop: 10 }}>Waktu Astronomical Twilight Berakhir</Text>
-            <Text style={{ color: '#000' }}>{astronomical_twilight_end}</Text>
-
+            <Text style={{ marginTop: 15 }}>Waktu Astronomical Twilight Berakhir</Text>
+            <Text style={{ color: '#000' }}>{this.displayTime('astronomical_twilight_end')}</Text>
           </View>
-          {/* </View> */}
         </Animated.View>
       </Modal>
     );
@@ -164,7 +170,8 @@ export default class ModalContent extends React.Component {
 const styles = StyleSheet.create({
   lineView: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginTop: 10
   },
   line: {
     flex: 1,
